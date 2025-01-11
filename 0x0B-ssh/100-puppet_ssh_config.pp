376263b2a3bc9526aeaa161041224d9a9d1b727d
NomceboLMncina <nomcebolungapearlmncina@gmail.com>
2025-01-11 11:39:35 -0800

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,5 +3,5 @@
 file { "ssh_config":
   path => "/etc/ssh/ssh_config",
   ensure => 'present',
-[2;2R[>77;30706;0c]10;rgb:bfbf/bfbf/bfbf]11;rgb:0000/0000/0000  content => "Host *\n\tIdentityFile ~/.ssh/school\t\nPasswordAuthentication no",
+  content => "Host *\n\tIdentityFile ~/.ssh/school\t\nPasswordAuthentication no",
 }
